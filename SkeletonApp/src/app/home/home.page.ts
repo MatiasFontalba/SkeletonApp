64cffe06b0c5d51b81fcc710a7ff799591b13627
MatiasFontalba <ma.fontalba@duocuc.cl>
2023-10-11 03:28:19 -0300

    Avance animaciones

--- SkeletonApp/src/app/home/home.page.ts
+++ SkeletonApp/src/app/home/home.page.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
+import { AnimationController } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-home',
@@ -15,21 +18,59 @@ export class HomePage implements OnInit{
 
   constructor(private route: ActivatedRoute,
              private formBuilder: FormBuilder,
-             private alertController:AlertController) {
+             private alertController:AlertController,
+             private animationCtrl: AnimationController) {
     this.datosForm=this.formBuilder.group({
       nombre: ['', Validators.required],
       apellido: ['', Validators.required],
       nivelEducativo: ['', Validators.required],
-      fechaNacimiento: ['', Validators.required]
+      fechaNacimiento: ['', [Validators.required, this.fechaValidaValidator()]]
     })
   }
 
   ngOnInit() {
     const usuario = this.route.snapshot.paramMap.get('usuario');
     this.usuarioRecibido = usuario !== null ? usuario : '';
+
+    this.startAnimation();
+    this.startAnimationInput();
   }
+
+   async startAnimation(){
+    //Animacion del titulo
+    console.log('Se está ejecutando la animación');
+    const titleElement = document.querySelector('.title');
+    if (titleElement){
+    const titleAnimation = this.animationCtrl.create()
+    .addElement(titleElement)
+    .duration(2500)
+    .iterations(Infinity)
+    .fromTo('opacity', 0.2, 1)//por lo qye entendi el primer numero es la opacidad y el segundo el tiempo
+    .fromTo('transform', 'translateY(100px)', 'translateX(100px)');
+    console.log('Se ejecuta la animacion');
+    
+    titleAnimation.play();
+    } else{
+	  console.log("no se encontró elemento en la clase title");
+    }
+  }
+    
+
   limpiarCampos(){
     this.datosForm.reset();
+  }
+
+  fechaValidaValidator(){
+    return (control: FormControl): {[ key: string]: boolean} | null => {
+      const fechaIngresada = new Date(control.value);
+      const fechaMinima = new Date('1920-01-01');
+      const fechaMaxima = new Date('2023-01-01');
+
+      if (isNaN(fechaIngresada.getTime()) || fechaIngresada < fechaMinima || fechaIngresada > fechaMaxima ){
+        return {'fechaInvalida': true};
+      }
+      return null;
+    };
   }
 
   async mostrarPopup() {
@@ -48,4 +89,24 @@ export class HomePage implements OnInit{
     }
   }
  }
+
+ async startAnimationInput(){
+  //Animacion del titulo
+  console.log('Input animacion');
+  const inputElement1 = document.querySelector('.input1');
+  const inputElement2 = document.querySelector('.input2');
+  if (inputElement1 && inputElement2){
+  const inputAnimation = this.animationCtrl.create()
+  .addElement(inputElement1)
+  .addElement(inputElement2)
+  .duration(1000)
+  .iterations(1)
+  .fromTo('transform', 'translateY(200px)', 'translateX(100px)');
+  console.log('Ejecucion de inputs');
+
+  inputAnimation.play();
+ } else{
+  console.log("no se encontró input");
+ }
+}
 }
